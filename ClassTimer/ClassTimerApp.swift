1b4e1d5ab4c30c346ab4fc599da9014bc00534fb
Stepan K <stepankravtsov0402@gmail.com>
2023-04-10 09:29:08 +0300

    Initial Commit

---
+++ ClassTimer/ClassTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassTimerApp.swift
+//  ClassTimer
+//
+//  Created by Степан Кравцов on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClassTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
